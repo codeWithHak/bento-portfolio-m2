506dce2306ce7f44792f45bdefd7c6686023d040
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-10-24 18:28:39 +0500

    cahnged animation text

--- src/components/ui/AnimatedText.tsx
+++ src/components/ui/AnimatedText.tsx
@@ -12,10 +12,10 @@ export default function AnimatedText() {
 
   return (
     <h2 className="text-center max-w-40 md:max-w-max ">
-      <span className={`inline-block ${animate ? 'animate-jump' : ''} `}>think</span>{' '}
-      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-300`}>design</span>{' '}
-      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-500`}>develop</span>{' '}
-      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-700`}>deploy</span>
+      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-200`}>think</span>{' '}
+      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-500`}>design</span>{' '}
+      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-700`}>develop</span>{' '}
+      <span className={`inline-block ${animate ? 'animate-jump' : ''} delay-1000`}>deploy</span>
     </h2>
   );
 }
