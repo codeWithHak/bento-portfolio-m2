49cd2779b82bc2e034a7a1d9917aab203e78a1a9
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-10-29 21:30:30 +0500

    fixed some bugs page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import MainGrid from "@/components/ui/MainGrid";
-import RetroGrid from "@/components/ui/retro-grid";
-import RetroGridBackground from "@/components/ui/RetroGridBackground";
-
-
 
 
 export default function Home() {
